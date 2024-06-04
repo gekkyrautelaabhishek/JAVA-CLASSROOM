c77d4e6b8a2d1d19d7ac7a51fc5b726150c1308d
gekkyrautelaabhishek <abhisheksinghrautela027@gmail.com>
2024-06-04 06:02:12 +0530

    Add these files

---
+++ datatypes.java
@@ -0,0 +1,17 @@
+public class datatypes {
+    public static void main(String[] args) {
+        //Primitive data types
+        byte age = 30;
+        int phone = 1283272;
+        long phone2 = 18293897323L;
+        float pi = 3.14F;
+
+        //Non-Primitive
+        String name = "Aman";
+        System.out.println(name.length());
+
+
+
+    }
+    
+}
---
+++ variables.java
@@ -0,0 +1,11 @@
+public class variables {
+    public static void main(String[] args) {
+        
+        //Variables
+        String name = "Aman";
+        String neighbour = "Akku";
+        int age = 23;
+        String friend = neighbour;
+    }
+    
+}
